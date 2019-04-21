4fe7a86742294d0fdb9f7843efbc40e0b911e60e
jongwankim <akim2009m@gmail.com>
2019-04-20 19:59:57 -0700

    Chapter 2 Exercise 2 complete

---
+++ CH2_2/CH2_2.java
@@ -0,0 +1,10 @@
+/* 
+ * Consider the nextInt method of the Scanner class. Is it an accessor or mutator? 
+ * Why? What about the nextInt method of the Random class 
+ */
+
+/* 
+ * The nextInt method of the class Scanner and Random are both 
+ * accessors because it does not modify the the object on which
+ * it was invoked.
+ */
