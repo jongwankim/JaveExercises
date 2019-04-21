7d96e6a4272eecef5ec970a5a62ee0c446646bc4
jongwankim <akim2009m@gmail.com>
2019-04-20 20:08:19 -0700

    Chapter 2 Exercise 3 complete

---
+++ CH2_3/CH2_3.java
@@ -0,0 +1,12 @@
+/* 
+ * Can you ever have a mutator method return something other than void? 
+ * Can you ever have an accessor method return void? 
+ */
+
+/* 
+ * Yes, you can have a mutator method return something, for example
+ * the ArrayList's add method returns a boolean along with modifying the 
+ * object it was called on.
+ * No, if a accessor method returns void it is a pointless method 
+ * because it neither modifies an object or return a value.
+ */
